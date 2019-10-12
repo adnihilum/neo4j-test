d3da31a2316522f09095a6a2e605216260252457
adnihilum <b1er.new@gmail.com>
2019-10-12 13:12:06 +0500

    move the application code into "app" directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
+    
+import Control.Monad.IO.Class
+import Data.Default
+import qualified Data.Text as T
+import qualified Database.Bolt as B
+import Database.Bolt (BoltActionT)
 
-import Lib
+main :: (MonadIO m) => m ()
+main = do
+  let config =
+        def
+          { B.host = "localhost"
+          , B.port = 7687
+          , B.user = "neo4j"
+          , B.password = "test"
+          }
+  pipe <- B.connect config
+  B.run pipe action
+  B.close pipe
+  return ()
 
-main :: IO ()
-main = someFunc
+action :: (MonadIO m) => BoltActionT m ()
+action = do
+  B.query "CREATE (p:Person)-[:LIKES]->(t:Technology)" >>= print_
+  B.query "MATCH (p:Person)-[:LIKES]-(t:Technology) RETURN p, t" >>= print_
+  return ()
+  where
+    print_ = liftIO . print
