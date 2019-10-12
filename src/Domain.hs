b261f5fe13473779199eaa49a5b8af4bddf5feaf
adnihilum <b1er.new@gmail.com>
2019-10-12 13:12:22 +0500

    add doman data types

---
+++ src/Domain.hs
@@ -0,0 +1,32 @@
+module Domain where
+
+data Molecule =
+  Molecule
+    { moleculeId :: Int
+    , moleculeSmiles :: String
+    , moleculeIupacName :: String
+    }
+
+data Reaction =
+  Reaction
+    { reactionId :: Int
+    , reactionName :: String
+    }
+
+data Catalyst =
+  Catalyst
+    { catalystId :: Int
+    , catalystSmiles :: String
+    , catalystName :: Maybe String
+    }
+
+data PRODUCT_FROM =
+  PRODUCT_FROM
+    { productFromAmount :: Float
+    }
+
+data ACCELERATE =
+  ACCELERATE
+    { accelerateTemperature :: Float
+    , acceleratePressure :: Float
+    }
